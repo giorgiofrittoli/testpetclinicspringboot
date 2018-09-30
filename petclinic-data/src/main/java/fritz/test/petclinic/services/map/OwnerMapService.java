30fc1a9f2916d2675d11531a895cebcb6135b80b
giorgiofrittoli <giorgio.frittoli@gmail.com>
2018-09-30 15:48:31 +0200

    implemented fynd by last name method

--- petclinic-data/src/main/java/fritz/test/petclinic/services/map/OwnerMapService.java
+++ petclinic-data/src/main/java/fritz/test/petclinic/services/map/OwnerMapService.java
@@ -11,7 +11,7 @@ import org.springframework.stereotype.Service;
 import java.util.Set;
 
 @Service
-@Profile({"map","default"})
+@Profile({"map", "default"})
 public class OwnerMapService extends AbstractMapService<Owner, Long> implements OwnerService {
 
 
@@ -26,7 +26,10 @@ public class OwnerMapService extends AbstractMapService<Owner, Long> implements 
 
 	@Override
 	public Owner findByLastName(String lastName) {
-		return null;
+		return super.findAll().stream().filter(owner -> owner.getLastName()
+				.equalsIgnoreCase(lastName))
+				.findFirst()
+				.orElse(null);
 	}
 
 	@Override
